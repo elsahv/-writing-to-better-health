213afea31a86c38debb8b1afa16ec19660589344
Elsa <elsahvy@gmail.com>
2023-05-15 20:14:16 -0700

    mobile adjust, about pg

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,12 +3,12 @@ import img2 from "../../public/images/self.jpg";
 
 const About = () => {
   return (
-    <div className="">
-      <div className="grid grid-cols-2">
-      <section className="">
+    <div className="h-screen 2xl:mt-[55px] 2xl:ml-[205px] lg:m-5 lg:h-100% overflow-x-hidden">
+      <div className="h-screen grid 2xl:grid-cols-2">
+      <section className="h-full p-3">  
         <h2 className="text-2xl">About</h2>
         <p>
-          Welcome to Writing to Better Health!! This site is currently a
+          Welcome to Writing to Better Health!! This site is currently a  
           work in progress, a comic blog regarding nutrition and mental
           wellness.
         </p>
@@ -16,8 +16,8 @@ const About = () => {
           {"I'm"} Elsa, a web developer who really, really likes food.
         </p>
       </section>
-      <section className="p-5 w-96 h-96">
-        <Image src={img2} alt="elsa-hovey" className="img" />
+      <section className="p-5 2xl:w-96 2xl:h-96 lg:p-1">
+        <Image src={img2} alt="elsa-hovey" className="img 2xl:w-full lg:w-1/2 md:w-1/2" />
       </section>
     </div>
     </div>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <div className="mt-[50px] 2xl:h-screen" id="contact">
     <div className="grid 2xl:grid-cols-2 lg:grid-cols-1 mt-5 mb-5 ">
     
-    <div className="pt-[55px] 2xl:pl-[105px] md:p-2 mx-2">
+    <div className="pt-[55px] 2xl:pl-[105px] mx-5 px-5 md:p-2">
           <h2 className="text-xl my-1">
             What do you think of this site so far?
           </h2>
@@ -15,15 +15,15 @@ const Contact = () => {
             developers, {"I'd"} really love to hear from ya!
           </p>
 
-          <p>
+          <p> 
             Simply fill out the contact form, or email me at
             <a
                 className="underline pl-2"
                 href="mailto:devdesignsbyelsa@gmail.com"
               >
                 devdesignsbyelsa@gmail.com
-              </a>0
-          </p>
+              </a>
+          </p>  
           </div>
       <section className="border-l-2 border-black-500">
         <div className="pt-5 mt-5 2xl:mx-[150px] lg:mx-[100px] md:mx-[50px] sm:mx-5">
